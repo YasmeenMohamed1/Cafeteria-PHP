b764d8ed56b83f1f12f56672d5b19714d45d05c3
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-03-30 16:16:04 +0200

    Update login.php

--- login.php
+++ login.php
@@ -21,9 +21,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($user) {
         if (password_verify($password, $user['password'])) {
-            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['id'] = $user['id'];
             $_SESSION['user_name'] = $user['name'];
-            $_SESSION['user_image'] = $user['image'];
+            $_SESSION['image'] = $user['image'];
 
             if ($user['role'] == 'admin') {
                 header("Location: adminHome.php");
