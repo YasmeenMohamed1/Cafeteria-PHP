7c9dce51271f35a2560a0addbd59b4831a2bce8a
Soad Ahmed <soadahmed1498@gmail.com>
2024-03-31 15:16:55 +0200

    catch data from product to set into cart

--- user/user.index.php
+++ user/user.index.php
@@ -1,20 +1,26 @@
 <?php
 session_start();
+// session_destroy();
 (@include("../layouts/header.php")) or die(" file not exist");
 (@include("../layouts/user.nav.php")) or die(" file not exist");
-if(true == 2){
-    echo 'true';
-}else{
-    echo 'false';
-}
+
 ?>
 
 <div class="container mt-5">
     <div class="row">
-        <div class="col-8 offset-4">
+    
+        <div class="col-4">
+        <h5 class="text-center">Cart Items</h5>
+           <?php 
+           include("../cart2/cart.php"); 
+        //    include("../cart.php"); 
+          
+           ?>
+        </div>
+        <div class="col-8">
             <!-- search -->
-            <h5 class="mb-3 lastproduct">last Products</h5>
-            <div class="d-flex flex-wrap mb-5" id=lastpro>
+            <h5 class="mb-3 lastproduct">Latest Order</h5>
+            <div class="d-flex flex-wrap mb-2" id=latestOrder>
 
             </div>
             <div id="lastpro-pagination"></div>
@@ -30,7 +36,9 @@ if(true == 2){
 
 
 <script>
-    function fetch_data(page) {
+    $(document).ready(function () {
+        fetch_data();
+        function fetch_data(page) {
         $.ajax({
             url: "user.controller.php",
             method: "POST",
@@ -43,36 +51,54 @@ if(true == 2){
         });
     }
 
-    fetch_data();
-
-    // Event delegation to handle click on dynamically loaded elements
-    $(document).on("click", ".proItem", function() {
-    var $this = $(this);
-    var $lastPro = $("#lastpro");
-
-    // Check if the clicked element is in lastpro
-    var isInLastPro = $this.closest($lastPro).length > 0;
-
-    // Check if the clicked element has class 'clicked'
-    var isClicked = $this.hasClass('clicked');
-
-    // If the clicked element is in lastpro and it doesn't have the class 'clicked', append it to lastpro
-    if (!isClicked) {
-        // Clone and append the element to lastpro
-        var $clonedProduct = $this.clone().appendTo($lastPro);
-
-        // Add class 'clicked' to the original element
-        $this.addClass('clicked');
-        
-    }
-    });
-
-
-
     $(document).on("click", ".page-item", function() {
         let page = $(this).attr("id");
         fetch_data(page);
     });
+
+    
+    // Event delegation to handle click on dynamically loaded elements
+    $(document).on("click", ".proItem", function() {
+        var id = $(this).find('.id').val();
+        var name = $(this).find('.name').val();
+        var price = $(this).find('.price').val();
+        var quantity = $(this).find('.quantity').val();
+        alert("id"+id+", name"+ name +", price"+price+ ", quantity"+quantity)
+        $.ajax({
+            method: "POST",
+            url: "../cart/add_to_cart.php",
+            data: {id:id , name:name , price:price , quantity:quantity},
+            success: function (response) {
+                alert("you have putted new item")
+            }
+        });  
+        var $this = $(this);
+        var $latestOrder = $("#latestOrder");
+
+        // Check if the clicked element is in latestOrder
+        var isInlatestOrder = $this.closest($latestOrder).length > 0;
+
+        // Check if the clicked element has class 'clicked'
+        var isClicked = $this.hasClass('clicked');
+
+        // If the clicked element is in lastpro and it doesn't have the class 'clicked', append it to lastpro
+        if (!isClicked) {
+            // Clone and append the element to lastpro
+            var $clonedProduct = $this.clone().appendTo($latestOrder);
+
+            // Add class 'clicked' to the original element
+            $this.addClass('clicked');
+            
+        }
+    });
+
+    });
+
+
+
+
+
+
 </script>
 
 <?php
