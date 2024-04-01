4c9e9ec3c2a5bcdb87bac507aa4af66af46d5503
Yomna khaled <yomnakhaled2001@gmail.com>
2024-04-01 13:48:42 +0200

    Update Layouts

--- layouts/footer.php
+++ layouts/footer.php
@@ -7,6 +7,20 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
  <!-- Core theme JS-->
 <script src="../js/scripts.js"></script>
+
+<script>
+    function updateExt(select) {
+        var selectedRoomNo = select.value;
+        var extInput = document.getElementById('exampleFormControlInput3');
+        // Update the disabled property of the input based on the selected room number
+        if (selectedRoomNo == 101 || selectedRoomNo == 102 || selectedRoomNo == 105 ) {
+            extInput.value = 1;
+        } else if(selectedRoomNo == 103 || selectedRoomNo == 104 ){
+            extInput.value = 2;
+        }
+    }
+</script>
+
 </body>
 
 </html>
--- layouts/header.php
+++ layouts/header.php
@@ -11,9 +11,22 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="../css/our_styles.css" rel="stylesheet" />
-        <link href="../css/user_styles.css" rel="stylesheet" />
+        <link href="css/our_styles.css" rel="stylesheet" />
+        <!-- <link href="../css/user_styles.css" rel="stylesheet" /> -->
         
+        <style>
+            /* Custom background color */
+            .custom-bg {
+            background-color: #faebd7; 
+            /* background-color: #F9E0BB; */
+            }
+            
+            .text-color_dark_cafe {
+            color: #884A39; 
+            }
+
+            
+        </style>
 
     </head>
     <body>
