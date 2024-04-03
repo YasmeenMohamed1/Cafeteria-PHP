150c347694e7a019040ebe9d2278d68fa9338f49
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-04-03 19:18:30 +0200

    Update admin.index.php

--- admin/admin.index.php
+++ admin/admin.index.php
@@ -65,7 +65,7 @@ $_SESSION['logout']= "../logout.php";
         $result = $database->get_connection()->query($sql);
         $result = $result->fetchAll(PDO::FETCH_ASSOC);
         foreach ($result as $row) {
-            echo '<option value="' . $row['user_id'] . '">' . $row['user_name'] . '</option>';
+           ?><option value="<?= $row["user_name"] ?>"><?= $row["user_name"] ?></option><?php
         }
         ?>
     </select>
